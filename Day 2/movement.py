97dc80b234a67ffa86f1fc8017e1648ffcc78a71
jonahsaidian <85853206+jonahsaidian@users.noreply.github.com>
2021-12-09 16:38:17 -0800

    Add files via upload

--- Day 2/movement.py
+++ Day 2/movement.py
@@ -1,21 +1,37 @@
 #%%
 import numpy as np
+import pandas as pd
 
 #%%
-input = np.loadtxt('./input.txt')
+input = pd.read_csv('./input.txt',header=None)
 # %%
-#1.1
-count=0
-for i in range(len(input)-1):
-    if input[i+1]>input[i]:
-        count+=1
+#2.1
+x=0
+y=0
+for i in range(len(input)):
+    if input.iloc[i][0][0]=='f':
+        x+=int(input.iloc[i][0][-1])
+    if input.iloc[i][0][0]=='u':
+        y+=-int(input.iloc[i][0][-1])
+    if input.iloc[i][0][0]=='d':
+        y+=int(input.iloc[i][0][-1])
 
-count
+print(x*y)
 # %%
 #1.2
-count=0
-for i in range(0,len(input)-3):
-    if input[i+3]>input[i]:
-        count+=1
-count
+x=0
+y=0
+aim=0
+for i in range(len(input)):
+    if input.iloc[i][0][0]=='f':
+        x+=int(input.iloc[i][0][-1])
+        y+=aim*int(input.iloc[i][0][-1])
+    if input.iloc[i][0][0]=='u':
+        #y+=-int(input.iloc[i][0][-1])
+        aim+=-int(input.iloc[i][0][-1])
+    if input.iloc[i][0][0]=='d':
+        #y+=int(input.iloc[i][0][-1])
+        aim+=int(input.iloc[i][0][-1])
+
+print(x*y)
 # %%
